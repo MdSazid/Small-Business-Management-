31136c79c09e3aa9e162198e2af93dadf8d81f0e
MdSazid <sazidhasan94@gmail.com>
2019-04-28 01:49:59 +0600

    first commit

---
+++ POSManagementProject.DLL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSManagementProject.DLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ POSManagementProject/Controllers/StockReportController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using POSManagementProject.DAL;
+using POSManagementProject.Models.ViewModels;
+using POSManagementProject.Report;
+
+namespace POSManagementProject.Controllers
+{
+    public class StockReportController : Controller
+    {
+        StockReportVM stockReportVm = new StockReportVM();
+        StockReportDAL stockReportDal = new StockReportDAL();
+
+        // GET: StockReport
+        public ActionResult Index()
+        {
+            
+            stockReportVm.SelectListBranch = stockReportDal.GetBranchSelectList();
+            stockReportVm.StockList = stockReportDal.GetStockList();
+            return View(stockReportVm);
+        }
+
+        [HttpPost]
+        public ActionResult Index(long branchId)
+        {
+            stockReportVm.SelectListBranch = stockReportDal.GetBranchSelectList();
+            stockReportVm.StockList = stockReportDal.GetStockList(branchId);
+            return View(stockReportVm);
+        }
+
+
+        public ActionResult ExportAllInfoToPdf(long branchId)
+        {
+            StockInfoExportToFile exPdf = new StockInfoExportToFile();
+            exPdf.ExportAllInfoToPdf(stockReportDal.GetStockList(branchId));
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
